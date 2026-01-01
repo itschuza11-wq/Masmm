4d3ad6b3516fc322255c058e2d42cf0c2c34b775
itschuza11-wq <itschuza11@gmail.com>
2026-01-02 01:52:03 +0500

    Update Orders.php

--- admin/Orders.php
+++ admin/Orders.php
@@ -1,2 +1,21 @@
-<?php include("../config/session.php"); ?>
+<?php
+include("../config/session.php");
+include("../config/db.php");
+
+if ($_SESSION['role'] != "admin") {
+    header("Location: ../index.php");
+    exit;
+}
+
+$q = mysqli_query($conn, "SELECT * FROM orders ORDER BY id DESC");
+?>
 <h3>All Orders</h3>
+
+<?php while($o = mysqli_fetch_assoc($q)): ?>
+<p>
+<b>#<?= $o['id'] ?></b> |
+<?= $o['service'] ?> |
+<?= $o['quantity'] ?> |
+<?= $o['status'] ?>
+</p>
+<?php endwhile; ?>
