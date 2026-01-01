e61546594ec2330672120957233338dc72033ed9
itschuza11-wq <itschuza11@gmail.com>
2026-01-02 02:05:37 +0500

    Add links to New Order and My Orders in dashboard

--- user/dashboard.php
+++ user/dashboard.php
@@ -1,12 +1,14 @@
 <?php
 include("../config/session.php");
 if($_SESSION['role']!="user"){
- header("Location: ../index.php");
- exit;
+  header("Location: ../index.php");
+  exit;
 }
 ?>
 <h3>User Dashboard</h3>
 <p>Balance: Rs 0</p>
+
 <a href="new-order.php">New Order</a><br>
 <a href="orders.php">My Orders</a><br>
 <a href="affiliate.php">Affiliate</a>
+
