3a3b6436bb3f68c6fdee0487b905cbe70384ccf8
itschuza11-wq <itschuza11@gmail.com>
2026-01-02 01:57:31 +0500

    Add link to 'New Order' in user dashboard

--- user/dashboard.php
+++ user/dashboard.php
@@ -7,5 +7,6 @@ if($_SESSION['role']!="user"){
 ?>
 <h3>User Dashboard</h3>
 <p>Balance: Rs 0</p>
+<a href="new-order.php">New Order</a><br>
 <a href="orders.php">My Orders</a><br>
 <a href="affiliate.php">Affiliate</a>
