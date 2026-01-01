9d6bffd18284048dda17ed0860ee28bcb0ca82f1
itschuza11-wq <itschuza11@gmail.com>
2026-01-02 02:12:53 +0500

    Update new-order.php

--- user/new-order.php
+++ user/new-order.php
@@ -1,50 +1,20 @@
-<?php
-include("../config/session.php");
-include("../config/db.php");
-
-if ($_SESSION['role'] != "user") {
-    header("Location: ../index.php");
-    exit;
-}
-
 if (isset($_POST['submit'])) {
 
-    $service  = mysqli_real_escape_string($conn, $_POST['service']);
-    $link     = mysqli_real_escape_string($conn, $_POST['link']);
-    $qty      = (int)$_POST['quantity'];
-    $price    = $qty * 0.5; // demo price
+    $service_id = (int)$_POST['service'];
+    $link = mysqli_real_escape_string($conn, $_POST['link']);
+    $qty  = (int)$_POST['quantity'];
+
+    // get service rate
+    $s = mysqli_fetch_assoc(
+        mysqli_query($conn, "SELECT rate FROM services WHERE id='$service_id'")
+    );
+
+    $price = ($qty / 1000) * $s['rate'];
 
     mysqli_query($conn, "
         INSERT INTO orders (user_id, service, link, quantity, price, status)
-        VALUES ('{$_SESSION['uid']}', '$service', '$link', '$qty', '$price', 'pending')
+        VALUES ('{$_SESSION['uid']}', '$service_id', '$link', '$qty', '$price', 'pending')
     ");
 
     $success = "Order placed successfully";
 }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>New Order</title>
-</head>
-<body>
-
-<h3>New Order</h3>
-
-<?php if(isset($success)) echo $success; ?>
-
-<form method="post">
-<select name="service">
-  <option>Instagram Followers</option>
-  <option>Instagram Likes</option>
-  <option>Facebook Likes</option>
-</select><br><br>
-
-<input name="link" placeholder="Profile / Post Link"><br><br>
-<input name="quantity" type="number" placeholder="Quantity"><br><br>
-
-<button name="submit">Place Order</button>
-</form>
-
-</body>
-</html>
