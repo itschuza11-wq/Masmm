7bc261de91b97cf98f024470bb5b340febe3691e
itschuza11-wq <itschuza11@gmail.com>
2026-01-03 00:02:27 +0500

    Update dashboard.php

--- user/dashboard.php
+++ user/dashboard.php
@@ -1,14 +1,22 @@
 <?php
 include("../config/session.php");
-if($_SESSION['role']!="user"){
-  header("Location: ../index.php");
-  exit;
+include("../config/db.php");
+
+if ($_SESSION['role'] != "user") {
+    header("Location: ../index.php");
+    exit;
 }
 ?>
+
 <h3>User Dashboard</h3>
+
 <p>Balance: Rs 0</p>
 
 <a href="new-order.php">New Order</a><br>
 <a href="orders.php">My Orders</a><br>
-<a href="affiliate.php">Affiliate</a>
+<a href="affiliate.php">Affiliate</a><br>
+<a href="add-funds.php">Add Funds</a><br>
 
+<hr>
+
+<a href="../index.php?logout=1">Logout</a>
