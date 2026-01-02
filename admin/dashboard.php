f17e660c4239bc3d256ed31b502c24f69a42ec7a
itschuza11-wq <itschuza11@gmail.com>
2026-01-03 00:46:47 +0500

    Add admin dashboard with user stats and links

---
+++ admin/dashboard.php
@@ -0,0 +1,32 @@
+<?php
+include("../config/session.php");
+include("../config/db.php");
+
+if ($_SESSION['role'] != "admin") {
+    header("Location: ../index.php");
+    exit;
+}
+
+// stats
+$users  = mysqli_fetch_row(mysqli_query($conn,"SELECT COUNT(*) FROM users"))[0];
+$orders = mysqli_fetch_row(mysqli_query($conn,"SELECT COUNT(*) FROM orders"))[0];
+$sales  = mysqli_fetch_row(mysqli_query($conn,"SELECT SUM(price) FROM orders"))[0];
+?>
+
+<h3>Admin Dashboard</h3>
+
+<div>Users: <?php echo $users; ?></div>
+<div>Orders: <?php echo $orders; ?></div>
+<div>Sales: Rs <?php echo $sales ?? 0; ?></div>
+
+<hr>
+
+<a href="users.php">Users</a><br>
+<a href="orders.php">Orders</a><br>
+<a href="services.php">Services</a><br>
+<a href="providers.php">API Providers</a><br>
+<a href="payments.php">Payments</a><br>
+
+<hr>
+
+<a href="../index.php?logout=1">Logout</a>
