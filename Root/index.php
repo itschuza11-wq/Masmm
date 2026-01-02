bd5079542efb118fc7f8769007f217820c14439a
itschuza11-wq <itschuza11@gmail.com>
2026-01-02 23:07:55 +0500

    Update index.php

--- Root/index.php
+++ Root/index.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
+
+/* ---------- LOGOUT ---------- */
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: index.php");
+    exit;
+}
+
 include("config/db.php");
 
+/* ---------- LOGIN ---------- */
 if (isset($_POST['login'])) {
 
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -16,14 +25,15 @@ if (isset($_POST['login'])) {
         $_SESSION['role']  = $user['role'];
         $_SESSION['uid']   = $user['id'];
 
-        if ($user['role'] == 'admin') {
+        if ($user['role'] == "admin") {
             header("Location: admin/dashboard.php");
         } else {
             header("Location: user/dashboard.php");
         }
         exit;
+
     } else {
-        $error = "Invalid login details";
+        $error = "Invalid email or password";
     }
 }
 ?>
@@ -31,14 +41,23 @@ if (isset($_POST['login'])) {
 <html>
 <head>
 <title>masmmpanel Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+
 <h3>Login</h3>
-<?php if(isset($error)) echo $error; ?>
+
+<?php if(isset($error)) echo "<p style='color:red'>$error</p>"; ?>
+
 <form method="post">
-<input name="email" placeholder="Email"><br><br>
-<input type="password" name="password" placeholder="Password"><br><br>
+<input type="email" name="email" placeholder="Email" required><br><br>
+<input type="password" name="password" placeholder="Password" required><br><br>
 <button name="login">Login</button>
 </form>
+
+<p>
+New user? <a href="signup.php">Create account</a>
+</p>
+
 </body>
 </html>
