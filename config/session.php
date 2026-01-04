e0dfa2a3ea6297b05df2423eecc6396621d48096
itschuza11-wq <itschuza11@gmail.com>
2026-01-05 01:57:34 +0500

    Update session.php

--- config/session.php
+++ config/session.php
@@ -1,5 +1,19 @@
 <?php
 session_start();
+require_once __DIR__ . "/db.php";
+?>
+$conn = mysqli_connect(
+  "sql309.infinityfree.com",
+  "if0_40818881",
+  "WuV4DzRiGA",
+  "if0_40818881_masmm"
+);
+
+if (!$conn) {
+  die("Database connection failed: " . mysqli_connect_error());
+}
+?>
+
 
 if (!isset($_SESSION['email'])) {
     header("Location: ../index.php");
